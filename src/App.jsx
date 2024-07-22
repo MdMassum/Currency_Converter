459553a970af2a6ff6538de53f07d56384fbb10d
Md Emamudin <mdemamudin726@gmail.com>
2024-07-22 16:24:04 +0530

    api integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
 
 import './App.css'
 import Comp from './Components/Comp'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
-    {/* <h1 className='bg-green-500'>hello</h1> */}
     <Comp/>
     </>
   )
--- src/Components/Comp.jsx
+++ src/Components/Comp.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
 import { VscArrowSwap } from "react-icons/vsc";
+import { useState, useEffect } from 'react';
+import countryList from '../assets/cntryCode'
 
 function Comp() {
+
+    // useStates -->
+    const [fromCntry, setFromCntry] = useState("USD");
+    const [toCntry, setToCntry] = useState("INR");
+    const [amnt, setAmnt] = useState(1);
+    const [finalAmnt, setFinalamnt] = useState(1);
+
+    const handleOnChange = (e) =>{
+
+        setAmnt(e.target.value);
+        
+    }
+
+    const fetchData=async()=>{
+
+        let from = fromCntry.toLowerCase();
+        let to = toCntry.toLowerCase();
+        let api_url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${from}.json`
+
+        let res = await fetch(api_url);
+        let data = await res.json();
+    
+        // console.log(data[from][to]);
+        let rate = data[from][to];
+        let convertedAmnt = (amnt * rate).toFixed(2);
+    
+        setFinalamnt(convertedAmnt);
+
+    }
+
+    useEffect(() => {
+
+        fetchData();
+
+    }, [amnt,fromCntry,toCntry])
+    
   return (
     <>
     <div className="p-10 rounded-3xl w-500 h-50vh bg-white text-black flex flex-col justify-center ">
@@ -9,15 +47,23 @@ function Comp() {
 
         <div className="mb-6">
             <p className='font-bold'>Enter Amount</p>
-            <input className='w-full' type="Number" />
+            <input className='w-full' type="Number" value={amnt} onChange={handleOnChange}/>
         </div>
 
         <div className="font-bold flex justify-center space-x-5 items-center mb-6">
             <div>
                 <p>From</p>
                 <div className='flex justify-center items-center border-solid border-black'>
-                    <img width={50} src="https://flagsapi.com/US/flat/64.png" alt=""/> 
-                    <select className='h-9' name="fromCntry" id=""></select>
+                    <img width={50} src={`https://flagsapi.com/US/flat/64.png`} alt={fromCntry}/> 
+                    <select className='h-9' value={fromCntry} onChange={(e)=>setFromCntry(e.target.value)}>
+                    {
+                        Object.entries(countryList).map(([name, code]) => (
+                        <option key={code} value={name}>
+                            {name}
+                        </option>
+                        ))
+                    }
+                    </select>
                 </div>
 
             </div>
@@ -28,18 +74,26 @@ function Comp() {
             <div>
                 <p>To</p>
                 <div className='flex justify-center items-center' >
-                    <img width={50} src="https://flagsapi.com/US/flat/64.png" alt=""/> 
-                    <select className='h-9'  name="fromCntry" id=""></select>
-                </div>
+                    <img width={50} src={`https://flagsapi.com/IN/flat/64.png`} alt={toCntry}/> 
+                    <select className='h-9' value={toCntry} onChange={(e)=>setToCntry(e.target.value)} >
+                    {
+                        Object.entries(countryList).map(([name, code]) => (
+                        <option key={code} value={name}>
+                            {name}
+                        </option>
+                        ))
+                    }
 
+                    </select>
+                </div>
             </div>
         </div>
 
         <div className='mb-7 font-bold text-xl'>
-            <p >1 USD = 78 INR</p>
+            <p >{amnt} {fromCntry} = {finalAmnt} {toCntry}</p>
         </div>
 
-        <button className='bg-blue-500 text-white'>Get Exchange Rate</button>
+        <button onClick={fetchData} className='bg-blue-500 text-white'>Get Exchange Rate</button>
        
     </div>
     </>
